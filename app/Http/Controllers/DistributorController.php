97f7d17ff4cc398440de6b504a7cd42c2984289d
gitajuliwanti <48758785+gitajuliwanti@users.noreply.github.com>
2019-06-13 11:11:16 +0700

    update

---
+++ app/Http/Controllers/DistributorController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Distributor;
+use Illuminate\Http\Request;
+
+class DistributorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('distributor.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $item= new Distributor;
+        $item->kode=$request['kode'];
+        $item->nama=$request['nama'];
+        $item->jekel=$request['jekel'];
+        $item->alamat=$request['alamat'];
+        $item->nohp=$request['nohp'];
+        $item->save();
+
+        return redirect()->back()->with('distributor', 'Data Tersimpan');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Distributor  $distributor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Distributor $distributor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Distributor  $distributor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Distributor $distributor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Distributor  $distributor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Distributor $distributor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Distributor  $distributor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Distributor $distributor)
+    {
+        //
+    }
+}
---
+++ resources/views/distributor/index.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+        @if(session('distributor'))
+            <div class="alert alert-success" role="alert">
+                {{session('distributor')}}
+            </div>
+        @endif
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    <form method="POST" action="/distributor/create">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="kode" class="col-md-4 col-form-label text-md-right">Kode Distributor</label>
+
+                            <div class="col-md-6">
+                                <input id="kode" type="text" class="form-control" name="kode" value="{{ old('email') }}">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="nama" class="col-md-4 col-form-label text-md-right">Nama</label>
+
+                            <div class="col-md-6">
+                                <input id="nama" type="text" class="form-control" name="nama" value="{{ old('email') }}">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="jekel" class="col-md-4 col-form-label text-md-right">Jenis Kelamin</label>
+
+                            <div class="col-md-6">
+                                <input id="jekel" type="text" class="form-control" name="jekel" value="{{ old('email') }}">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="alamat" class="col-md-4 col-form-label text-md-right">Alamat</label>
+
+                            <div class="col-md-6">
+                                <input id="alamat" type="text" class="form-control" name="alamat" value="{{ old('email') }}">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="nohp" class="col-md-4 col-form-label text-md-right">Nomor HP</label>
+
+                            <div class="col-md-6">
+                                <input id="nohp" type="text" class="form-control" name="nohp" value="{{ old('email') }}">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Insert
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
